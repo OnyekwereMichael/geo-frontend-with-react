45c88ca071b33203e45d00f63c51a02a8e515b47
Onyekwere Michael <onyekweremichael55@gmail.com>
2024-03-14 14:00:51 +0100

    Geo Location Tracker

--- src/map/osm.js
+++
@@ -1,7 +0,0 @@
-export default {
-    maptiler: {
-      url: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=KyviE1am2Be8NSlcSVWW",
-      attribution: '&copy; <a href="https://www.maptiler.com/">Maptiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }
-  };
-  
